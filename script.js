dd762a630062fcb7a3025c2852066d9b828766eb
JenniferCastro <jennifer.erc@gmail.com>
2022-08-04 17:03:54 -0400

    exercicios em aula

---
+++ script.js
@@ -0,0 +1,39 @@
+// aula de condicionais - JS - Labenu
+
+// if(bool1){
+//     alert('entrou no if, bool1 é true')
+// }else
+
+
+// let idade = 18
+
+// if(idade >= 60 || idade >= 16 && idade < 18) {
+//     console.log('Título facultativo')
+// }else if(idade >=18){
+//     console.log('Tem direito ao título de leitor')
+// }else{
+//     console.log('Você não tem a idade mínima par o titulo de eleitor')
+// }
+
+//exercicio 1 /2 
+let media = 11
+if(media >= 5){
+        console.log('Aprovado')
+    }else if(media >= 3){
+        console.log('Recuperação')
+    }else if(media < 3){
+        console.log('Reprovado')
+    }else{
+        console.log('Dado inválido')
+    }
+
+ 
+    //exercicio 2
+
+
+//     if(idade >= 60 || idade >= 16 && idade < 18) {
+//     console.log('Título facultativo')
+// }else if(idade >=18){
+//     console.log('Tem direito ao título de leitor')
+// }else{
+    // console.log('Você não tem a idade mínima par o titulo de eleitor')*/
